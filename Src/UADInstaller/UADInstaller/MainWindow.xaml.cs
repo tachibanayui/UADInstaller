01d3dc65563ea3c1e0ec2fcd602da7e8a366b426
quangaming2929 <quangaming2929@DESKTOP-AJC0EMN>
2019-05-14 21:14:16 +0700

    Add simple view to UADInstaller

--- Src/UADInstaller/UADInstaller/MainWindow.xaml.cs
+++ Src/UADInstaller/UADInstaller/MainWindow.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using MahApps.Metro.Controls;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -18,11 +22,22 @@ namespace UADInstaller
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow, INotifyPropertyChanged
     {
+        public List<Product> AllProducts { get; set; } = new List<Product>();
+        public ObservableCollection<Product> InstalledProduct { get; set; } = new ObservableCollection<Product>();
+
         public MainWindow()
         {
+            DataContext = this;
+            AllProducts.Add(new Product() { Name = "Universal Anime Downloader", Description = "Searching and downloading anime films easier. In addition, the program's built-in video player has a lot of unique features to enhance the user's experience.", Version = "v0.9.0", IsButtonEnable = false });
             InitializeComponent();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
